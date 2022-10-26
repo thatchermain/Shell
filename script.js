3917e4830a5c9dd2aca0000fe6c7f9d55b7d35d1
thatchermain <dominikkrzysztalowicz@gmail.com>
2022-10-26 18:37:09 +0200

    Final fix

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 let picture = document.querySelector(".hidden");
+let clip = document.querySelector(".clip");
+let video = document.querySelector(".video");
 
 changeClass = () => {
   console.log("clicked");
   picture.classList.add("overlay");
   picture.classList.remove("hidden");
+  clip.classList.add("gone");
+  clip.classList.remove("clip");
+  video.classList.add("video-bg");
 };
 
-const wait = setTimeout(changeClass, 7650);
+const wait = setTimeout(changeClass, 7680);
